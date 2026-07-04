10115e4e482ebc9d23fc77d2849487980ed1e69b
shinyatlas <mannyoso07@gmail.com>
2026-07-04 03:51:26 -0400

    added tests checking greek output

--- tests/bsm_tests.cpp
+++ tests/bsm_tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "BSMModel.hpp"
+#include "Greeks.hpp"
 
 TEST(BSMModelTest, CallPrice) {
     double S = 100.0;   // Spot price
@@ -29,4 +30,21 @@ TEST(BSMModelTest, PutPrice) {
     double expectedPutPrice = 5.5735; 
 
     EXPECT_NEAR(putPrice, expectedPutPrice, 1e-4);
+}
+
+TEST(BSMModelTest, CallGreeks) {
+    double S = 100.0;   // Spot price
+    double K = 100.0;   // Strike price
+    double T = 1.0;     // Time to maturity in years
+    double r = 0.05;    // Risk-free interest rate
+    double sigma = 0.2; // Volatility of the underlying asset
+
+    BSMModel bsm(S, K, T, r, sigma);
+    Greeks callGreeks = bsm.callGreeks();
+
+    EXPECT_NEAR(callGreeks.delta, 0.6368, 1e-4);
+    EXPECT_NEAR(callGreeks.gamma, 0.0188, 1e-4);
+    EXPECT_NEAR(callGreeks.vega, 37.5240, 1e-4);
+    EXPECT_NEAR(callGreeks.theta, -6.4140, 1e-4);
+    EXPECT_NEAR(callGreeks.rho, 53.2325, 1e-4);
 }
