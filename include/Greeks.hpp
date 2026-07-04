f875e624e18a99aa11a6387a16f366748d1c3a66
shinyatlas <mannyoso07@gmail.com>
2026-07-04 03:45:01 -0400

    created Greeks struct for future greeks calculations

---
+++ include/Greeks.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// All major Greeks are included in this struct. Add more Greeks as needed.
+struct Greeks {
+    double delta;
+    double gamma;
+    double vega;
+    double theta;
+    double rho;
+};
