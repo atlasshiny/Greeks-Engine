f484d6cca51f9cd9b339217f38f765ba9bc54d33
shinyatlas <mannyoso07@gmail.com>
2026-07-04 19:29:05 -0400

    added GPU macro for future cuda port

--- include/math/normcdf.hpp
+++ include/math/normcdf.hpp
@@ -1,6 +1,15 @@
 #pragma once
 #include <cmath>
 
-inline double normcdf(double x) {
-    return 0.5 * (1.0 + std::erf(x / std::sqrt(2.0)));
+// GPU macro for CUDA compatibility
+#ifdef __CUDACC__
+    #define HOST_DEVICE __host__ __device__
+#else
+    #define HOST_DEVICE
+#endif
+
+namespace MathUtils {
+    HOST_DEVICE inline double normcdf(double x) {
+        return 0.5 * (1.0 + std::erf(x / std::sqrt(2.0)));
+    }
 }
--- include/math/normpdf.hpp
+++ include/math/normpdf.hpp
@@ -2,6 +2,18 @@
 #include <cmath>
 #include <numbers>
 
-inline double normpdf(double x) {
-    return std::exp(-0.5 * x * x) / std::sqrt(2 * std::numbers::pi);
+// GPU macro for CUDA compatibility
+#ifdef __CUDACC__
+    #define HOST_DEVICE __host__ __device__
+#else
+    #define HOST_DEVICE
+#endif
+
+// Define PI as a constant using std::numbers::pi
+inline constexpr double PI = std::numbers::pi;
+
+namespace MathUtils {
+    HOST_DEVICE inline double normpdf(double x) {
+        return std::exp(-0.5 * x * x) / std::sqrt(2 * PI);
+    }
 }
