aafaa05f29b73ad0b2277c292d40a47dc79351d2
shinyatlas <mannyoso07@gmail.com>
2026-07-06 19:40:42 -0400

    fixed bash script version to take into account compiler differences

--- benchmarks/run_benchmark.sh
+++ benchmarks/run_benchmark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Running CPU Benchmark..."
-../build/benchmarks/Release/cpu_benchmark --benchmark_out=cpu_results.csv --benchmark_out_format=csv
+../build/benchmarks/cpu_benchmark --benchmark_out=cpu_results.csv --benchmark_out_format=csv
 
 echo "Running GPU Benchmark..."
-../build/benchmarks/Release/gpu_benchmark --benchmark_out=gpu_results.csv --benchmark_out_format=csv
+../build/benchmarks/gpu_benchmark --benchmark_out=gpu_results.csv --benchmark_out_format=csv
 
 echo "Benchmarks complete. Results saved to cpu_results.csv and gpu_results.csv."
