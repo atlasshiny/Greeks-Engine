d138663edc11e2a30c91103faf225263177102e1
shinyatlas <mannyoso07@gmail.com>
2026-07-06 04:20:26 -0400

    added timing to the cudaMemcpy and output it as a counter

--- benchmarks/GPUBenchmark.cu
+++ benchmarks/GPUBenchmark.cu
@@ -5,9 +5,11 @@
 #include <cuda_runtime.h>
 
 static void BM_GPU_BSM(benchmark::State& state) {
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+    cudaEvent_t start_compute, stop_compute, start_copy, stop_copy;
+    cudaEventCreate(&start_compute);
+    cudaEventCreate(&stop_compute);
+    cudaEventCreate(&start_copy);
+    cudaEventCreate(&stop_copy);
 
     int n = static_cast<int>(state.range()); // Get the number of test cases from the benchmark state
 
@@ -18,31 +20,70 @@ static void BM_GPU_BSM(benchmark::State& state) {
     launchGreeksKernel(data.options.data(), data.results.data(), warmup_size);
     cudaDeviceSynchronize(); // Ensure the warmup fully completes
 
+    // Assign the memory outside the lauchGreeksKernel method to benchmark JUST the calculations
+
+    Option *d_options;
+    Greeks *d_results;
+
+    // Allocate memory on GPU
+    cudaMalloc(&d_options, n * sizeof(Option));
+    cudaMalloc(&d_results, n * sizeof(Greeks));
+
     for (auto _ : state) {
-        // Generate test inputs
+        // Generate test inputs and copy it to the GPU
         BenchmarkBatch data = generateBenchmarkBatch(n); // Generate n test cases
+
+        // Record copy start
+        cudaEventRecord(start_copy, 0);
+
+        // Copy memory from RAM to VRAM
+        cudaMemcpy(d_options, data.options.data(), n * sizeof(Option), cudaMemcpyHostToDevice);
+        
+        // Record copy stop
+        cudaEventRecord(stop_copy, 0);
+        cudaEventSynchronize(stop_copy);
+
+        // Find total elapsed time
+        float copy_ms = 0;
+        cudaEventElapsedTime(&copy_ms, start_copy, stop_copy);
+
+        // Calculate grid dimensions
+        int threadsPerBlock = 256;
+        int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
 
         // Record CUDA start
         cudaDeviceSynchronize();
-        cudaEventRecord(start, 0);
+        cudaEventRecord(start_compute, 0);
 
-        launchGreeksKernel(data.options.data(), data.results.data(), n);
+        // Run the raw kernel without the bridge function
+        computeGreeksKernel<<<blocksPerGrid, threadsPerBlock>>>(d_options, d_results, n);
+        
+        // Ensure the calculations are done before stopping the time
+        cudaDeviceSynchronize();
 
         // Record CUDA stop
-        cudaEventRecord(stop, 0);
-        cudaEventSynchronize(stop);
+        cudaEventRecord(stop_compute, 0);
+        cudaEventSynchronize(stop_compute);
 
         // Find total elapsed time
         float milliseconds = 0;
-        cudaEventElapsedTime(&milliseconds, start, stop);
+        cudaEventElapsedTime(&milliseconds, start_compute, stop_compute);
 
         // Give time to Google Benchmark
         state.SetIterationTime(milliseconds / 1000.0);
+
+        // Add cudaMemcpy time to the output
+        state.counters["Memcpy_GPU_ms"] = copy_ms;
+
     }
 
-    cudaEventDestroy(start);
-    cudaEventDestroy(stop);
+    cudaEventDestroy(start_compute);
+    cudaEventDestroy(stop_compute);
 
-}//This macro registers the benchmark, sets the range for the data, and forces the use of the cudaEvent time
+    cudaFree(d_options);
+    cudaFree(d_results);
+}
+
+//This macro registers the benchmark, sets the range for the data, and forces the use of the cudaEvent time
 BENCHMARK(BM_GPU_BSM)->Range(100, 10000000)->UseManualTime(); 
 BENCHMARK_MAIN();
