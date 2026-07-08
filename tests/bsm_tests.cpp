e7bcede8d88171885b306c856339c0c94ba2b058
shinyatlas <mannyoso07@gmail.com>
2026-07-08 14:40:03 -0400

    Added a test to verify greeks for put options

--- tests/bsm_tests.cpp
+++ tests/bsm_tests.cpp
@@ -48,3 +48,20 @@ TEST(BSMModelTest, CallGreeks) {
     EXPECT_NEAR(callGreeks.theta, -6.4140, 1e-4);
     EXPECT_NEAR(callGreeks.rho, 53.2325, 1e-4);
 }
+
+TEST(BSMModelTest, PutGreeks) {
+    double S = 100.0;   // Spot price
+    double K = 100.0;   // Strike price
+    double T = 1.0;     // Time to maturity in years
+    double r = 0.05;    // Risk-free interest rate
+    double sigma = 0.2; // Volatility of the underlying asset
+
+    BSMModel bsm(S, K, T, r, sigma);
+    Greeks putGreeks = bsm.calculateGreeks(1); // 1 for put option
+
+    EXPECT_NEAR(putGreeks.delta, -0.3632, 1e-4);
+    EXPECT_NEAR(putGreeks.gamma, 0.0188, 1e-4);
+    EXPECT_NEAR(putGreeks.vega, 37.5240, 1e-4);
+    EXPECT_NEAR(putGreeks.theta, -1.7865, 1e-4);
+    EXPECT_NEAR(putGreeks.rho, -46.7675, 1e-4);
+}
