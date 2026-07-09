b566d23374f3a9aa1257e09270cf3b319dd9893c
shinyatlas <mannyoso07@gmail.com>
2026-07-08 20:22:34 -0400

    added kernel and helper function for pricing the options on the GPU

--- src/GreeksKernel.cu
+++ src/GreeksKernel.cu
@@ -5,7 +5,7 @@
 #include "gpu/GreeksKernel.cuh"
 #include "gpu/error_checking.cuh"
 
-// The kernel that executes the code on the GPU
+// The kernel that executes the Greeks code on the GPU
 __global__ void computeGreeksKernel(const Option* options, Greeks* results, int n) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < n) {
@@ -14,6 +14,19 @@ __global__ void computeGreeksKernel(const Option* options, Greeks* results, int 
         
         // Calculate Greeks using the header-file code
         results[i] = model.calculateGreeks(options[i].type);
+
+    }
+}
+
+// The kernel that executes the pricing code on the GPU
+__global__ void computePriceKernel(const Option* options, double* results, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        // Instantiate the header-only BSMModel on the GPU thread
+        BSMModel model(options[i].S, options[i].K, options[i].T, options[i].r, options[i].sigma);
+        
+        // Calculate Greeks using the header-file code
+        results[i] = model.price(options[i].type);
 
     }
 }
@@ -42,6 +55,35 @@ void launchGreeksKernel(const Option* h_options, Greeks* h_results, int n) {
 
     // Copy results back to host
     CUDA_CHECK(cudaMemcpy(h_results, d_results, n * sizeof(Greeks), cudaMemcpyDeviceToHost));
+
+    // Free GPU memory
+    CUDA_CHECK(cudaFree(d_options));
+    CUDA_CHECK(cudaFree(d_results));
+}
+
+void launchPricingKernel(const Option* h_options, double* h_results, int n) {
+    Option *d_options;
+    double *d_results;
+
+    // Allocate memory on GPU
+    CUDA_CHECK(cudaMalloc(&d_options, n * sizeof(Option)));
+    CUDA_CHECK(cudaMalloc(&d_results, n * sizeof(double)));
+
+    // Copy data to GPU
+    CUDA_CHECK(cudaMemcpy(d_options, h_options, n * sizeof(Option), cudaMemcpyHostToDevice));
+
+    // Calculate grid dimensions
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
+
+    // Launch Kernel on GPU
+    computePriceKernel<<<blocksPerGrid, threadsPerBlock>>>(d_options, d_results, n);
+
+    // Check for errors after launching the kernel
+    CUDA_CHECK(cudaGetLastError());
+
+    // Copy results back to host
+    CUDA_CHECK(cudaMemcpy(h_results, d_results, n * sizeof(double), cudaMemcpyDeviceToHost));
 
     // Free GPU memory
     CUDA_CHECK(cudaFree(d_options));
