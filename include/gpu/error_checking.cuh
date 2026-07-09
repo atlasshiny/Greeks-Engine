93db4107e2e10b14ce7c771bd31702e145ca8e0f
shinyatlas <mannyoso07@gmail.com>
2026-07-09 19:22:21 -0400

    made the check function inline to avoid multiple definition errors

--- include/gpu/error_checking.cuh
+++ include/gpu/error_checking.cuh
@@ -5,7 +5,7 @@
 
 #define CUDA_CHECK(val) check((val), #val, __FILE__, __LINE__)
 
-void check(cudaError_t err, const char* func, const char* file, int line) {
+inline void check(cudaError_t err, const char* func, const char* file, int line) {
     if (err != cudaSuccess) {
         std::cerr << "CUDA Runtime Error at: " << file << ":" << line << std::endl;
         std::cerr << func << " failed with error " << cudaGetErrorString(err) << std::endl;
