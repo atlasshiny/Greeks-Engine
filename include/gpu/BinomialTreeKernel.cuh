501782cd72fc997fccaea93f2dbfc9ae02d009d5
shinyatlas <mannyoso07@gmail.com>
2026-07-09 15:07:01 -0400

    added MarketParameter struct as a parameter to the functions that need it

--- include/gpu/BinomialTreeKernel.cuh
+++ include/gpu/BinomialTreeKernel.cuh
@@ -1,15 +1,16 @@
 #pragma once
 #include "Option.hpp"
+#include "MarketParameters.hpp"
 #include "Greeks.hpp"
 
 // The kernel that executes the Greeks calculation code on the GPU
-__global__ void computeGreeksKernel(const Option* options, Greeks* results, int n);
+__global__ void computeGreeksKernel(const Option* options, const MarketParams* mktparams, Greeks* results, int n_steps, int n_options);
 
 // The Bridge Function for the Greeks calculation: Orchestrates memory and execution
-void launchGreeksKernel(const Option* h_options, Greeks* h_results, int n);
+void launchGreeksKernel(const Option* h_options, const MarketParams* h_mktparams, Greeks* h_results, int n_steps, int n_options);
 
 // The kernel that executes the pricing code on the GPU
-__global__ void computePricingKernel(const Option* option, double* results, int n);
+__global__ void computePricingKernel(const Option* option, const MarketParams* mktparams, double* results, int n_steps, int n_options);
 
 // The Bridge Function for the pricing calculation: Orchestrates memory and execution
-void launchPricingKernel(const Option* h_options, double* h_results, int n);
+void launchPricingKernel(const Option* h_options, const MarketParams* h_mktparams, double* h_results, int n_steps, int n_options);
