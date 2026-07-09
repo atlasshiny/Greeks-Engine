4c0c71c13fd90429660b363f9aaff14e50344254
shinyatlas <mannyoso07@gmail.com>
2026-07-09 14:43:07 -0400

    added isAmerican bool to the Option struct and assigned a value to it where it is constructed

--- benchmarks/setup.hpp
+++ benchmarks/setup.hpp
@@ -12,8 +12,8 @@ struct BenchmarkBatch {
 
 BenchmarkBatch generateBenchmarkBatch(size_t n) {
     BenchmarkBatch data;
-    data.options = std::vector<Option>(n, {100.0, 1.0, 0}); // Example: Spot=100, Strike=100, T=1 year, r=5%, sigma=20%, type=Call
-    data.mktparams = std::vector<MarketParams>(n, {100.0, 0.05, 0.2, 0.0});
+    data.options = std::vector<Option>(n, {100.0, 1.0, 0, false}); // Example: Strike=100, T=1 year, type=Call, isAmerican=false
+    data.mktparams = std::vector<MarketParams>(n, {100.0, 0.05, 0.2, 0.0}); // Example: Spot=100, r=5%, sigma=20%, q=0%
     data.results = std::vector<Greeks>(n); // Preallocate results
 
     return data;
--- include/Option.hpp
+++ include/Option.hpp
@@ -5,4 +5,5 @@ struct Option {
     double T; // Time to maturity in years
 
     int type; // Option type: 0 for Call, 1 for Put
+    bool isAmerican; // Whether or not a given option is an American option (used for Binomial Tree pricing)
 };
