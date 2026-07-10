a388d8793c39bd0b7635a0143cc8ca00b8471fc9
shinyatlas <mannyoso07@gmail.com>
2026-07-09 21:29:51 -0400

    Updated both benchmarks to support binomial tree and the new method names, signatures, and parameters for both models

--- benchmarks/CPUBenchmark.cpp
+++ benchmarks/CPUBenchmark.cpp
@@ -1,8 +1,11 @@
 #include "models/BSMModel.hpp"
+#include "models/BinomialTreeModel.hpp"
 #include "setup.hpp"
 #include <benchmark/benchmark.h>
 
-static void BM_CPU_BSM(benchmark::State& state) {
+// BSM BENCHMARKS
+
+static void BM_CPU_BSM_Greeks(benchmark::State& state) {
 
     int n = static_cast<int>(state.range()); // Get the number of test cases from the benchmark state
 
@@ -27,6 +30,41 @@ static void BM_CPU_BSM(benchmark::State& state) {
         }
     }
 }
+BENCHMARK(BM_CPU_BSM_Greeks)->Range(100, 10000000); // Benchmark for 100 to 10,000,000 options
 
-BENCHMARK(BM_CPU_BSM)->Range(100, 10000000);
+// BINOMIAL TREE BENCHMARKS
+
+static void BM_CPU_BinomialTreeGreeks(benchmark::State& state) {
+
+    int n_options = static_cast<int>(state.range()); // Get the number of test cases from the benchmark state
+    int n_steps = 200; // Number of steps in the binomial tree
+
+    // WARM-UP BEFORE BENCHMARKING
+    constexpr int warmup_size = 1000;
+    constexpr int warmup_steps = 50; // Use fewer steps for warm-up to speed up the process
+    BenchmarkBatch warmup_data = generateBenchmarkBatch(warmup_size);
+    std::vector<double> warmup_buffer(warmup_steps + 1); // Buffer size is warmup_steps + 1
+
+    for (size_t i = 0; i < warmup_data.options.size(); ++i) {
+        BinomialTreeModel model(warmup_data.mktparams[i].S, warmup_data.options[i].K, warmup_data.options[i].T, warmup_data.mktparams[i].r, warmup_data.mktparams[i].sigma, warmup_data.mktparams[i].q, warmup_steps, warmup_data.options[i].isAmerican);
+        model.calculateGreeks(warmup_data.options[i].type, warmup_buffer.data(), 0.01); // Calculate the Greeks (not stored, just for warm-up)
+    }
+
+    // Generate test inputs
+    BenchmarkBatch data = generateBenchmarkBatch(n_options); // Generate n test cases
+
+    // Create a buffer for the binomial tree calculations to avoid dynamic memory allocation during benchmarking
+    std::vector<double> buffer(n_steps + 1); // Buffer size is n_steps + 1
+
+    for (auto _ : state) {
+        // Run the Binomial Tree model on each option and store the results
+        for (size_t i = 0; i < data.options.size(); ++i) {
+            BinomialTreeModel model(data.mktparams[i].S, data.options[i].K, data.options[i].T, data.mktparams[i].r, data.mktparams[i].sigma, data.mktparams[i].q, n_steps, data.options[i].isAmerican);
+
+            model.calculateGreeks(data.options[i].type, buffer.data(), 0.01); // Calculate the Greeks (not stored, just for benchmarking)
+        }
+    }
+}
+BENCHMARK(BM_CPU_BinomialTreeGreeks)->Range(100, 100000); // Benchmark for 100 to 100,000 options
+
 BENCHMARK_MAIN();
--- benchmarks/GPUBenchmark.cu
+++ benchmarks/GPUBenchmark.cu
@@ -1,10 +1,13 @@
 #include "models/BSMModel.hpp"
 #include "setup.hpp"
 #include "gpu/BSMKernel.cuh"
+#include "gpu/BinomialTreeKernel.cuh"
 #include <benchmark/benchmark.h>
 #include <cuda_runtime.h>
 
-static void BM_GPU_BSM(benchmark::State& state) {
+// BSM BENCHMARKS
+
+static void BM_GPU_BSM_Greeks(benchmark::State& state) {
     cudaEvent_t start_compute, stop_compute, start_copy, stop_copy;
     cudaEventCreate(&start_compute);
     cudaEventCreate(&stop_compute);
@@ -17,7 +20,7 @@ static void BM_GPU_BSM(benchmark::State& state) {
     constexpr int warmup_size = 1000;
     BenchmarkBatch warmup_data = generateBenchmarkBatch(warmup_size);
 
-    launchBSMGreeksKernel(warmup_data.options.data(), warmup_data.mktparams.data(), warmup_data.results.data(), warmup_size);
+    launchBSMGreeksKernel(warmup_data.options.data(), warmup_data.mktparams.data(), warmup_data.greek_results.data(), warmup_size);
     cudaDeviceSynchronize(); // Ensure the warmup fully completes
 
     // Assign the memory outside the lauchGreeksKernel method to benchmark JUST the calculations
@@ -84,9 +87,102 @@ static void BM_GPU_BSM(benchmark::State& state) {
     cudaEventDestroy(stop_compute);
 
     cudaFree(d_options);
+    cudaFree(d_mktparams);
     cudaFree(d_results);
 }
-
 //This macro registers the benchmark, sets the range for the data, and forces the use of the cudaEvent time
-BENCHMARK(BM_GPU_BSM)->Range(100, 10000000)->UseManualTime(); 
+BENCHMARK(BM_GPU_BSM_Greeks)->Range(100, 10000000)->UseManualTime(); // Benchmark for 100 to 10,000,000 options
+
+// BINOMIAL TREE BENCHMARKS
+
+static void BM_GPU_BinomialTree_Greeks(benchmark::State& state) {
+    cudaEvent_t start_compute, stop_compute, start_copy, stop_copy;
+    cudaEventCreate(&start_compute);
+    cudaEventCreate(&stop_compute);
+    cudaEventCreate(&start_copy);
+    cudaEventCreate(&stop_copy);
+
+    int n_options = static_cast<int>(state.range()); // Get the number of test cases from the benchmark state
+    int n_steps = 200; // Number of steps in the binomial tree
+
+    // WARM-UP BEFORE BENCHMARKING
+    constexpr int warmup_size = 1000;
+    constexpr int warmup_step = 10;
+    BenchmarkBatch warmup_data = generateBenchmarkBatch(warmup_size);
+
+    launchBinomialGreeksKernel(warmup_data.options.data(), warmup_data.mktparams.data(), warmup_data.greek_results.data(), warmup_step, warmup_size);
+    cudaDeviceSynchronize(); // Ensure the warmup fully completes
+
+    // Assign the memory outside the lauchGreeksKernel method to benchmark JUST the calculations
+
+    Option *d_options;
+    MarketParams *d_mktparams;
+    double *d_buffer;
+    Greeks *d_results;
+
+    // Allocate memory on GPU
+    cudaMalloc(&d_options, n_options * sizeof(Option));
+    cudaMalloc(&d_mktparams, n_options * sizeof(MarketParams));
+    cudaMalloc(&d_buffer, n_options * (n_steps + 1) * sizeof(double));
+    cudaMalloc(&d_results, n_options * sizeof(Greeks));
+
+    // Generate test inputs (Still need to be copied to GPU)
+    BenchmarkBatch data = generateBenchmarkBatch(n_options); // Generate n test cases
+
+    for (auto _ : state) {
+        // Record copy start
+        cudaEventRecord(start_copy, 0);
+
+        // Copy memory from RAM to VRAM
+        cudaMemcpy(d_options, data.options.data(), n_options * sizeof(Option), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_mktparams, data.mktparams.data(), n_options *sizeof(MarketParams), cudaMemcpyHostToDevice);
+
+        // Record copy stop
+        cudaEventRecord(stop_copy, 0);
+        cudaEventSynchronize(stop_copy);
+
+        // Find total elapsed time
+        float copy_ms = 0;
+        cudaEventElapsedTime(&copy_ms, start_copy, stop_copy);
+
+        // Calculate grid dimensions
+        int threadsPerBlock = 256;
+        int blocksPerGrid = (n_options + threadsPerBlock - 1) / threadsPerBlock;
+
+        // Record CUDA start
+        cudaDeviceSynchronize();
+        cudaEventRecord(start_compute, 0);
+
+        // Run the raw kernel without the bridge function
+        computeBinomialGreeksKernel<<<blocksPerGrid, threadsPerBlock>>>(d_options, d_mktparams, d_results, n_steps, n_options, d_buffer);
+        
+        // Ensure the calculations are done before stopping the time
+        cudaDeviceSynchronize();
+
+        // Record CUDA stop
+        cudaEventRecord(stop_compute, 0);
+        cudaEventSynchronize(stop_compute);
+
+        // Find total elapsed time
+        float milliseconds = 0;
+        cudaEventElapsedTime(&milliseconds, start_compute, stop_compute);
+
+        // Give time to Google Benchmark
+        state.SetIterationTime(milliseconds / 1000.0);
+
+        // Add cudaMemcpy time to the output
+        state.counters["Memcpy_GPU_ms"] = copy_ms;
+
+    }
+
+    cudaEventDestroy(start_compute);
+    cudaEventDestroy(stop_compute);
+
+    cudaFree(d_options);
+    cudaFree(d_mktparams);
+    cudaFree(d_buffer);
+    cudaFree(d_results);
+}
+BENCHMARK(BM_GPU_BinomialTree_Greeks)->Range(100, 100000)->UseManualTime(); // Benchmark for 100 to 100,000 options
+
 BENCHMARK_MAIN();
