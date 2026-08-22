b13d5bbec9e0bfff6f27328caef1b9c4e54c7fba
shinyatlas <mannyoso07@gmail.com>
2026-08-22 11:49:02 -0400

    added a DEVICE macro for methods that are only called on device

--- include/macros.hpp
+++ include/macros.hpp
@@ -3,8 +3,10 @@
 // Macro for CUDA compatibility
 #ifdef __CUDACC__
     #define HOST_DEVICE __host__ __device__
+    #define DEVICE __device__
 #else
     #define HOST_DEVICE
+    #define DEVICE
 #endif
 
 // Macro for switching from std math functions to CUDA math functions when compiling with NVCC
