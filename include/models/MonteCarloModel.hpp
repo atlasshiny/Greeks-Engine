3bf6fe90b29aebe2bc9d7b0aa39056afd1900de3
shinyatlas <mannyoso07@gmail.com>
2026-08-22 12:23:11 -0400

    added a price method to MonteCarloModel, uniquely defined using a macro to check if cuda is available in the compiler

--- include/models/MonteCarloModel.hpp
+++ include/models/MonteCarloModel.hpp
@@ -1,15 +1,36 @@
 #pragma once
 #include "macros.hpp"
+#include "math/gbm.hpp"
 
 class MonteCarloModel {
 public:
     HOST_DEVICE MonteCarloModel(double S, double K, double T, double r, double sigma, double q, int numSimulations)
         : S(S), K(K), T(T), r(r), sigma(sigma), q(q), numSimulations(numSimulations) {}
 
-    HOST_DEVICE inline double price() {
-        // Placeholder for Monte Carlo pricing logic
-        return 0.0;
-    };
+    #ifdef __CUDACC__
+        // Device-side price call for CUDA threads
+        DEVICE inline double price(int optionType, curandState_t* state) const {
+            double totalPayoff = 0.0;
+            for (long i = 0; i < numSimulations; ++i) {
+                double ST = gbm.sampleTerminal(T, state);
+                double payoff = (optionType == 0) ? fmax(ST - K, 0.0) : fmax(K - ST, 0.0);
+                totalPayoff += payoff;
+            }
+            return std::exp(-r * T) * (totalPayoff / numSimulations);
+        }
+    #else
+        // Host-side price call for CPU
+        template <typename RNG>
+        inline double price(int optionType, RNG& gen, std::normal_distribution<double>& dist) const {
+            double totalPayoff = 0.0;
+            for (long i = 0; i < numSimulations; ++i) {
+                double ST = gbm.sampleTerminal(T, gen, dist);
+                double payoff = (optionType == 0) ? std::max(ST - K, 0.0) : std::max(K - ST, 0.0);
+                totalPayoff += payoff;
+            }
+            return std::exp(-r * T) * (totalPayoff / numSimulations);
+        }
+    #endif
 
     // Note the lack of a calculateGreeks method. If they were to be calculated, they would likely be calculated using 
     // finite differences similar to the BinomialTreeModel class. Because it is substantially more expensive to calculate Greeks 
@@ -23,10 +44,6 @@ private:
     double sigma;  // Volatility of the underlying asset
     double q;      // Dividend yield
     int numSimulations; // Number of Monte Carlo simulations
-
-    HOST_DEVICE inline double simulatePath() const {
-        // Placeholder for simulating a single path of the underlying asset price
-        return 0.0;
-    }
+    MathUtils::GBMProcess gbm{S, r, sigma}; // GBM process for simulating stock prices
 
 };
